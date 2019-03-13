49b8af5f132e5cf895840906d08358991a126a8e
Sean Biganski <sean@hellolabs.com>
2019-03-12 23:37:17 -0400

    Initial commit

---
+++ Assets/Scripts/Bucket.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bucket : MonoBehaviour
+{
+    public GameObject successCanvas;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with object: " + collision.gameObject.name);
+        successCanvas.SetActive(true);
+    }
+}
